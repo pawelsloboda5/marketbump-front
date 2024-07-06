935a28f89857106c2b636f67b7bab3ad5de412c8
Pawel Sloboda <pawelsloboda5@gmail.com>
2024-07-06 09:18:36 -0700

    BASE URL fix

--- app/components/LoginForm.tsx
+++ app/components/LoginForm.tsx
@@ -22,12 +22,13 @@ const LoginForm = ({ onRegisterClick }: { onRegisterClick: () => void }) => {
 
             if (response.ok) {
                 console.log('Login successful');
-                const sessionStatus = checkSession();
+                const sessionStatus = await checkSession();
                 if (sessionStatus.loggedIn) {
                     router.push('/user-profile');
                 }else{
                     console.log('Session not logged in');
                 }
+
             } else {
                 console.log('Login failed');
             }
@@ -42,7 +43,7 @@ const LoginForm = ({ onRegisterClick }: { onRegisterClick: () => void }) => {
         });
         const data = await response.json();
         console.log('Session status:', data);
-        return data;
+        return data.loggedIn;
     };
 
     return (
